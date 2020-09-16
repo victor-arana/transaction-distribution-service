e92d3c17b8bae583b9274a6a589bcef6a1419e76
victor-arana <victor_arana@hotmail.com>
2020-09-16 18:25:07 -0500

    :tada: create-project, initializes project using spring initializer
    Create a spring boot project using the spring initializer at
    start.spring.io using the following options:
    
    Project: Maven project
    Language: Java
    Spring Boot: 2.3.3
    
    Metadata:
    Group: no.data
    Artifact: transaction-distribution-service
    Name: Transaction Distribution Service
    Package name: no.data
    Packaging: Jar
    Java: 8
    
    Dependencies:
    - Spring Web
    Changes committed:
    	new file:   .gitignore
    	new file:   pom.xml
    	new file:   src/main/java/no/data/TransactionDistributionServiceApplication.java
    	new file:   src/main/resources/application.properties
    	new file:   src/test/java/no/data/TransactionDistributionServiceApplicationTests.java

---
+++ src/main/java/no/data/TransactionDistributionServiceApplication.java
@@ -0,0 +1,13 @@
+package no.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionDistributionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionDistributionServiceApplication.class, args);
+	}
+
+}
